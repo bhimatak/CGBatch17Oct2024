94846fdb75ba059828506ebf3a28c53638b3948c
bhimatak <bhima.t@gmail.com>
2024-11-04 10:11:25 +0530

    updated with day07

---
+++ day07/p1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main()
+{
+	int i;
+	float f;
+	char ch;
+	char str1[20];
+	double d;
+
+
+	printf("\nEnter the proper values\n");
+	printf("\nEnter interger value: ");
+	scanf("%3d",&i);
+
+	printf("\nInterger value: %05d\n",i);
+
+	return 0;
+}
