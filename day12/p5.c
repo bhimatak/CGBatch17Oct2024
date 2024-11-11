899e8e9cba0480f087442d87ecc4e86f0f320c82
bhimatak <bhima.t@gmail.com>
2024-11-11 17:45:16 +0530

    updated

--- day12/p5.c
+++ day12/p5.c
@@ -19,13 +19,19 @@ int getEmp(EMP *);
 
 int main()
 {
-	EMP e1;
+	int noEmp;
+	int i;
 
 	EMP *e=NULL;
+	EMP *temp=NULL;
 
 	// e = &e1;
 
-	e = (EMP *)malloc(sizeof(EMP));
+	printf("\nEnter the Number of Employee Strength: ");
+	scanf("%d",&noEmp);
+
+	e = (EMP *)malloc(noEmp*sizeof(EMP));
+	temp = e;
 
 /*
 	scanf("%d%d%d",&e1.id,&e1.sal,&e1.phno);
@@ -33,20 +39,45 @@ int main()
 	getchar();
 	scanf("%c",&e1.Gender);
 */
+	for(i=0;i<noEmp;i++,e++)
+	{
+			getEmp(e);
 
+	}
+
+	e = temp;
+	
+	for(i=0;i<noEmp;i++,e++)
+		printEmp(e);
+
+
+	
+	printf("\n\n");
+	free(e);
+
+	return 0;
+
+}
+
+
+int getEmp(EMP *e)
+{
+	printf("\nEnter the Employee Details\n");
 	scanf("%d%d%d",&e->id,&e->sal,&e->phno);
 	scanf("%s",e->Name);
 	getchar();
 	scanf("%c",&e->Gender);
+}
 
-
+int printEmp(EMP *e)
+{
+	printf("\nEmployee Details are\n");
+	printf("\n===============================\n");
 	printf("\nID: %d",e->id);
 	printf("\nName: %s",e->Name);
 	printf("\nGender: %c",e->Gender);
 	printf("\nPhNo: %d",e->phno);
 	printf("\nSalary: %d",e->sal);
-
-	printf("\n\n");
-	return 0;
+	printf("\n===============================\n");
 
 }
